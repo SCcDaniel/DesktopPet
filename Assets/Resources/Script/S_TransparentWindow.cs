16a17801c28c64064c5a468f9c5e6df830ad5cdf
hbbwang <hbbwang@tencent.com>
2023-07-04 20:14:37 +0800

    add Unity Project.

---
+++ Assets/Resources/Script/S_TransparentWindow.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using System;
+using System.Drawing;
+
+#if UNITY_STANDALONE_WIN
+using System.Runtime.InteropServices;
+using WindowTray;
+#endif
+
+public class S_TransparentWindow : MonoBehaviour
+{   
+    
+    public int WindowWidth = 512;
+    public int WindowHeight = 512;
+    public float Opacity = 1.0f;
+    public string TrayIconPath;
+    //导入Win32 api
+#if UNITY_STANDALONE_WIN
+    [DllImport("user32")]
+    private static extern int  GetWindowLong(IntPtr hwnd, int nIndex);
+    
+    [DllImport("user32")]
+    private static extern int  SetWindowLong(IntPtr hwnd, int nIndex , int dwNewLong);
+    
+    [DllImport("user32")]
+    private static extern IntPtr FindWindow(string lpClassName , string lpWindowName);
+
+    [DllImport("user32")]
+    private static extern int SetWindowPos(IntPtr hWnd,int hWndInsertAfter,int x, int y,int cx,int cy,int uFlags);
+
+    [DllImport("user32")]
+    private static extern int MessageBox(IntPtr hWnd,string lpText, string lpCaption, uint uType);
+
+    //分层窗口透明度 WS_EX_LAYERED
+    //crKey:需要剔除的背景颜色
+    //bAlpha:整体透明度
+    //dwFlags透明方式
+    [DllImport("user32.dll")]
+    static extern int SetLayeredWindowAttributes(IntPtr hwnd, int crKey, int bAlpha, int dwFlags);
+    private IntPtr hwnd;
+
+    //Style
+    private const int WS_BORDER = 0x00800000;	//窗口具有细线边框
+    private const int WS_CAPTION = 0x00C00000;//窗口具有标题栏。
+    //Style EX
+    private const int WS_EX_TRANSPARENT= 0x00000020;
+    private const int WS_EX_LAYERED = 0x00080000;//完全穿透模式
+    private const int WS_EX_TOOLWINDOW = 0x00000080;
+    //
+    private const int SWP_SHOWWINDOW = 0x0040;
+    //托盘
+    S_WindowTray Tray;
+
+    private void InitWindowStyle()
+    {
+        var appName = UnityEngine.Application.productName;
+        hwnd = FindWindow(null,appName);
+        if(hwnd == null)
+        {
+            MessageBox(IntPtr.Zero ,"查找Unity窗口出错...","错误",0);
+            return;
+        }
+        //窗口样式
+        int windowStyle = GetWindowLong(hwnd,-16);
+        int windowStyleEx = GetWindowLong(hwnd,-20);
+        SetWindowLong(hwnd,-16, windowStyle & ~WS_BORDER & ~WS_CAPTION);
+        SetWindowLong(hwnd,-20, windowStyleEx | WS_EX_LAYERED | WS_EX_TOOLWINDOW);
+        SetWindowPos(hwnd, -1, 0, 0, WindowWidth, WindowHeight, SWP_SHOWWINDOW);
+        SetLayeredWindowAttributes(hwnd, 0, (int)(Opacity*255.0f), 0x1 | 0x2);
+        //任务栏右下角托盘小图标
+        Tray = new S_WindowTray();
+        string iconPath = Application.streamingAssetsPath + TrayIconPath;
+        //Debug.Log(iconPath);
+        Tray.InitTray(ref hwnd, iconPath);
+    }
+
+#endif
+
+    void Awake()
+    {
+#if !UNITY_EDITOR
+        InitWindowStyle();
+#endif
+    }
+
+}
---
+++ Assets/Resources/Script/S_WindowSubIcon.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Unity.VisualScripting;
+
+namespace WindowTray
+{
+    public class S_WindowTray
+    {
+        //NotifyIcon 设置托盘相关参数
+        private NotifyIcon _notifyIcon = new NotifyIcon();
+        //托盘图标的宽高
+        private int iconSize = 40;
+        //做托盘图标的图片，这里用了.png格式
+        private IntPtr hwnd;
+
+        private MenuItem exit;
+
+        //调用该方法将运行程序显示到托盘 
+        public void InitTray(ref IntPtr inHwnd , string iconPath)
+        {
+            hwnd = inHwnd;
+            _notifyIcon.Text = "诗酱";
+            //托盘图标
+            if (iconPath.Length < 2)
+                _notifyIcon.Icon = new System.Drawing.Icon(SystemIcons.Warning, iconSize, iconSize);
+            else
+                _notifyIcon.Icon = CustomTrayIcon(iconPath, iconSize, iconSize);
+            _notifyIcon.ShowBalloonTip(2000);//托盘气泡显示时间
+            _notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu();
+            //退出按钮
+            exit = new MenuItem("退出", ExitApp);
+            _notifyIcon.ContextMenu.MenuItems.Add(exit);
+            Show();
+            _notifyIcon.ShowBalloonTip(2500, "", "你好呀.", ToolTipIcon.None);
+            Form.ActiveForm.ShowInTaskbar = false;
+            //Form.FromHandle(hwnd).FindForm().ShowInTaskbar=false;
+        }
+        /// 设置程序托盘图标
+        private Icon CustomTrayIcon(string iconPath, int width, int height)
+        {
+            Bitmap bt = new Bitmap(iconPath);
+            Bitmap fitSizeBt = new Bitmap(bt, width, height);
+            return Icon.FromHandle(fitSizeBt.GetHicon());
+        }
+        public void Show()
+        {
+            _notifyIcon.Visible = true;//托盘按钮是否可见
+        }
+
+        public void Hide()
+        {
+            _notifyIcon.Visible = false;//托盘按钮是否可见
+        }
+
+        private void ExitApp(object sender, EventArgs e)
+        {
+            Hide();
+            _notifyIcon.Dispose();
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            UnityEngine.Application.Quit(0);
+            #endif
+        }
+    }
+}
+
